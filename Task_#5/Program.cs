17dcad06f7e45e49a17216c77bb045b0df6a8144
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-24 10:42:58 +0300

    Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1,1); и т.д.)

---
+++ Task_#5/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+находящихся на главной диагонали (с индексами (0,0); (1,1); и т.д.)
+Например, задан массив:
+1 4 7 2
+4 9 2 3
+8 4 2 4
+*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+        if (int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+    else
+            Console.WriteLine("Введено не число. Повторите ввод");
+    return result;
+}
+
+int[,] InitArray(int m, int n)
+{
+    int[,] newArray = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArray[i, j] = rnd.Next(2, 10);
+        }
+    }
+    return newArray;
+}
+
+int GetSummaOfDiagonal(int[,] matrix)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                count += matrix[i, j];
+            }
+        }
+        
+    }
+    return count;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int row = GetNumber("Введите количество строк: ");
+int column = GetNumber("Введите количество столбцов: ");
+int[,] matrix = InitArray(row, column);
+PrintMatrix(matrix);
+Console.WriteLine($"Сумма чисел в диагонали = {GetSummaOfDiagonal(matrix)}");
