16dc73a64b6f9c674387f630d30ce8e3a9f98da0
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-24 10:41:18 +0300

    Как распечатать весь массив сразу

---
+++ Task_#2/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Как распечатать весь массив сразу
+*/
+
+int[] array = new int[10];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+    array[i] = rnd.Next(1, 10);
+
+string result = string.Join(", ", array);
+Console.WriteLine(result);
